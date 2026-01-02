e8ad7e1bdf2e5d3b166c59f7cf8eab7da862f787
CFAT <shifatsrm09@gmail.com>
2026-01-02 13:07:53 +0600

    prev clean

--- A6/A.java
+++ A6/A.java
@@ -1,6 +1,6 @@
 import java.io.*;
 import java.util.*;
-public class AP {
+public class A {
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -40,6 +40,9 @@ public class AP {
                 
             }
         }
+
+        System.out.println("Hello World");
+
 
         if (count == N) {
             System.out.println(sb.toString());
